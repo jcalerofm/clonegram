8261b0e4fa828e36228d8dd8ccae12ffd703e6c9
Jorge Calero <jorge.calero@gmail.com>
2020-03-15 21:33:57 +0100

    created models and validations

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   root to: 'pages#home'
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
