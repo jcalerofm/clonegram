5238fc6e44d4651c92f5d702fcc867bcfdfef13e
Jorge Calero <jorge.calero@gmail.com>
2020-03-15 11:05:44 +0100

    user show style

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/home'
   root to: 'pages#home'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
