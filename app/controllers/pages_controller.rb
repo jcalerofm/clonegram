51ff7408bc4c1afbf878b985d06129d1f8a69a65
Jorge Calero <jorge.calero@gmail.com>
2020-03-25 10:25:39 +0100

    post controller update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
-  def home
+  def index
+    if current_user
+      @posts = Post.order(created_at: :desc).page(params[:page]).per(5)
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
   def create
     Post.create(post_params)
     redirect_to root_path
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+
+    redirect_to user_path(current_user)
   end
 
   private
